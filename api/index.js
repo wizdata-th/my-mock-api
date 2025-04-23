bbe279c8e9b662bccf84f48d6437a64791503fbe
wizdata-th <wizdata.official@gmail.com>
2025-04-24 00:09:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 const db = require('../db.json');
 
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+
   if (req.method === 'POST') {
-    const newUser = req.body;
-    db.users.push(newUser);
-    return res.status(201).json(newUser);
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    
+    req.on('end', () => {
+      const newData = JSON.parse(body);
+      db.users.push(newData);
+      res.status(201).json(newData);
+    });
+    
+  } else {
+    res.json(db);
   }
-  
-  res.json(db);
 };
